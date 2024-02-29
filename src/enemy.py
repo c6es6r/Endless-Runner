753f209533355793e65b04902d27ec6cb75bd4f4
c6es6r <julius.meyers@cusgymerk.onmicrosoft.com>
2024-02-29 14:22:08 +0100

    enemy

---
+++ src/enemy.py
@@ -0,0 +1,24 @@
+import pygame
+
+import config
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.speed = 128
+        self.position = pygame.Vector2(config.SCREEN_WIDTH, config.SCREEN_HEIGHT/1.25)
+        self.velocity = pygame.Vector2()
+        self.image = pygame.image.load("img/enemy.png")
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.base = self.image
+        self.rect = self.base.get_rect(center=self.position)
+
+    def movement(self):
+        self.velocity = pygame.Vector2()
+        self.position.x -= self.speed
+
+        # check out of bounds
+        if self.position.x > config.SCREEN_WIDTH:
+            pass
+
+        self.rect = self.base.get_rect(center=self.position)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import math
 
 import config
 import player
+import enemy
 
 pygame.init()
 
@@ -10,12 +11,15 @@ screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 player = player.Player()
+enemies = [enemy.Enemy()]
 
 running = True
 
 def draw():
     sprites = pygame.sprite.Group()
     sprites.add(player)
+    for i in enemies:
+        sprites.add(i)
     sprites.draw(screen)
 
 while running:
@@ -26,6 +30,12 @@ while running:
     screen.blit(pygame.image.load("img/background.png"), (0, 0))
 
     player.movement()
+
+    for i in enemies:
+        if i.position.x < 0:
+            enemies.remove(i)
+
+        i.movement()
 
     draw()
 
