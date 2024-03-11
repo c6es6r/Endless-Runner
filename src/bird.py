a55c85034cd6dac629a069c3a523b64fe1988b44
c6es6r <julius.meyers@cusgymerk.onmicrosoft.com>
2024-03-11 20:25:21 +0100

    increasing movement speed

--- src/bird.py
+++ src/bird.py
@@ -6,7 +6,7 @@ import config
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.speed = 3
+        self.speed = 4
         self.position = pygame.Vector2(config.SCREEN_WIDTH-8, random.randint(10, 200))
         self.image = pygame.image.load("img/bird.png")
         self.base = self.image
