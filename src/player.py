cfc3c9d6e3d1085c1501034970b499a99ad9bc5d
thegrieferknight <stephanieherfs3@gmail.com>
2024-03-11 21:12:12 +0100

    fixed the jumping

--- src/player.py
+++ src/player.py
@@ -1,40 +1,42 @@
 import pygame
-
 import config
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.jump_power = 190
-        self.position = pygame.Vector2(config.SCREEN_WIDTH/6, config.COLLISION_HEIGHT)
+        self.jump_power = 19
+        self.gravity = 0.8
+        self.position = pygame.Vector2(config.SCREEN_WIDTH/6,
+                                       config.COLLISION_HEIGHT)
         self.velocity = pygame.Vector2()
         self.image = pygame.image.load("img/player.png")
         self.image = pygame.transform.scale(self.image, (64, 64))
         self.base = self.image
         self.rect = self.base.get_rect(center=self.position)
-
-        self.current_keys = []
-        self.previous_keys = []
+        self.jumnping = True
+        
+        self.keys = []
 
         self.lives = 1
         self.score = 0
 
     def movement(self):
-        self.velocity = pygame.Vector2()
-        self.velocity.y += 4
+        self.velocity.y += self.gravity
 
-        self.current_keys = pygame.key.get_pressed()
+        self.keys = pygame.key.get_pressed()
 
-        if (self.current_keys[pygame.K_SPACE] or self.current_keys[pygame.K_UP]) and self.previous_keys[pygame.K_SPACE] == False and self.position.y >= config.COLLISION_HEIGHT:
-            self.velocity.y -= self.jump_power
+        if (self.keys[pygame.K_UP] or self.keys[pygame.K_SPACE]) and self.jumping is False:
+            self.velocity.y = -self.jump_power
+            self.jumping = True
 
-        if self.current_keys[pygame.K_DOWN] or self.current_keys[pygame.K_s]:
+        if self.keys[pygame.K_DOWN] or self.keys[pygame.K_s]:
             self.velocity.y += 4
 
-        # check out of bounds
+        self.position += self.velocity
+
         if self.position.y > config.COLLISION_HEIGHT:
             self.position.y = config.COLLISION_HEIGHT
+            self.velocity.y = 0
+            self.jumping = False
 
-        self.position += self.velocity
         self.rect = self.base.get_rect(center=self.position)
-        self.previous_keys = self.current_keys
